43ad461010273050f0dd9520406bf0750e0d0ccd
pipll <gennadiy.kozlenko@gmail.com>
2017-02-02 23:56:32 +0300

    Update node-video-lib to v1.0.0

--- routes/vod/index.js
+++ routes/vod/index.js
@@ -19,7 +19,7 @@ function openMovie(req, res, next) {
         return fs.openAsync(fileName, 'r').then(fd => {
             req.file = fd;
             let movie = VideoLib.MP4Parser.parse(fd);
-            req.fragments = movie.fragments(config.fragmentDuration);
+            req.fragmentList = VideoLib.FragmentListBuilder.build(movie, config.fragmentDuration);
             next();
         }).finally(() => {
             if (req.file !== null) {
@@ -32,10 +32,12 @@ function openMovie(req, res, next) {
 }
 
 router.get(/^(.*)\/playlist\.m3u8$/, openMovie, (req, res) => {
+    let duration = req.fragmentList.relativeDuration();
+    let bandwidth = duration > 0 ? 8 * fs.fstatSync(req.file).size / duration : 0;
     let playlist = [
         '#EXTM3U',
         '#EXT-X-VERSION:3',
-        `#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=${req.fragments.bandwidth() << 0},RESOLUTION=${req.fragments.resolution()}`,
+        `#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=${bandwidth << 0},RESOLUTION=${req.fragmentList.resolution()}`,
         path.join(req.baseUrl, req.params[0], 'chunklist.m3u8').replace(/\\/g, '/')
     ];
     res.header('Content-Type', 'application/x-mpegURL');
@@ -46,11 +48,11 @@ router.get(/^(.*)\/chunklist\.m3u8$/, openMovie, (req, res) => {
     let playlist = [
         '#EXTM3U',
         '#EXT-X-VERSION:3',
-        `#EXT-X-TARGETDURATION:${req.fragments.fragmentDuration}`,
+        `#EXT-X-TARGETDURATION:${req.fragmentList.fragmentDuration}`,
         '#EXT-X-MEDIA-SEQUENCE:1'
     ];
-    for (let i = 0, l = req.fragments.length; i < l; i++) {
-        playlist.push(`#EXTINF:${_.round(req.fragments[i].relativeDuration(), 2)},`);
+    for (let i = 0, l = req.fragmentList.count(); i < l; i++) {
+        playlist.push(`#EXTINF:${_.round(req.fragmentList.get(i).relativeDuration(), 2)},`);
         playlist.push(path.join(req.baseUrl, req.params[0], `media-${i + 1}.ts`).replace(/\\/g, '/'));
     }
     playlist.push('#EXT-X-ENDLIST');
@@ -60,10 +62,10 @@ router.get(/^(.*)\/chunklist\.m3u8$/, openMovie, (req, res) => {
 
 router.get(/^(.*)\/media-(\d+)\.ts$/, openMovie, (req, res) => {
     let index = parseInt(req.params[1], 10);
-    if (req.fragments.length < index) {
+    if (req.fragmentList.count() < index) {
         throw new errors.NotFoundError('Chunk not found');
     }
-    let fragment = req.fragments[index - 1];
+    let fragment = req.fragmentList.get(index - 1);
     let buffer = VideoLib.HLSPacketizer.packetize(fragment, req.file);
     res.header('Content-Type', 'video/MP2T');
     res.send(buffer);
